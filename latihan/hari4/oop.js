c97832c951034e27129ad72e3a97e9a7e5985c29
xttiqa <attiqalucky@gmail.com>
2025-10-10 10:32:01 +0800

    memperbarui oop.js

--- latihan/hari4/oop.js
+++ latihan/hari4/oop.js
@@ -11,8 +11,34 @@ class Car {
     }
 }
 
-var innovam = new Car("Innovam", "Toyotwo")
+var innovam = new Car("Innovam", "Toyotwooogit")
 innovam.color = "blue"
 console.log(innovam.color)
 console.log(innovam.present())
 console.log(innovam)
+
+
+
+class Orang{
+    constructor(nama){
+        this.nama = nama;
+        this.age = ""
+    }
+
+    berjalan(){
+        return "TAP TAP TAP"
+    }
+}
+
+class Guru extends Orang {
+    constructor(nama){
+        super(nama);
+        this.school = ""
+    }
+}
+
+var guru = new Guru('Abidin')
+guru.school = "SD 24"
+guru.age = 24
+console.log(guru)
+console.log(guru.berjalan())
