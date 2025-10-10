8cc8fc5a936a94cf25eeba6406fc585ed1cc3cff
xttiqa <attiqalucky@gmail.com>
2025-10-10 09:44:13 +0800

    membuat file oop.js dari folder hari4

---
+++ latihan/hari4/oop.js
@@ -0,0 +1,10 @@
+class Car {
+    constructor(brand, factory){
+        this.brand = brand
+        this.factory = factory
+        this.sound = "vroom! vroom!"
+    }
+}
+
+var innovam = new Car("Innovam", "Toyotwo")
+console.log(innovam)
