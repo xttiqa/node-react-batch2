db51a674e285a12c4b5fe099eb70fa22318cf960
xttiqa <attiqalucky@gmail.com>
2025-10-13 16:28:21 +0800

    memperbarui file contoh.txt dan jawaban.txt

--- latihan/hari5/app.js
+++ latihan/hari5/app.js
@@ -2,15 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let movies = [
+    {id: 1, title: "Spider-Man", year: 2002},
+    {id: 2, title: "John Wick", year: 2014},
+    {id: 3, title: "The Avengers", year: 2012},
+    {id: 4, title: "Logan", year: 2017},
+]
+
+const getMovies = (req, res) => {
+    // res.send('Disini akan tampil data movie')
+    let result = " "
+
+    movies.forEach((item, index) => {
+        result += `<i>${index+1}. ${item.title}</i><br>`
+    })
+
+    res.json(movies) // Disarankan pake res.json(movies)
+}
+
 // ROOT
 app.get('/', (req, res) => {
-  res.send('Ohayou! Konichiwa! Oyasumi!')
+    res.send('Ohayou! Konichiwa! Oyasumi!')
 })
 
-// ROUTE
-app.get('/home', (req, res) => {
-  res.send('This is home!')
-})
+// ROUTE MOVIE
+app.get('/movie', getMovies)
 
 // ROUTE
 app.get('/peserta', (req, res) => {
@@ -22,6 +38,7 @@ app.get('/peserta/tika', (req, res) => {
   res.send('This is my page!')
 })
 
+app.use(express.static('public'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
