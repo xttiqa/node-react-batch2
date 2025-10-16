930f52c917ceea1590c31d840a72fff3ce986771
xttiqa <attiqalucky@gmail.com>
2025-10-16 14:21:39 +0800

    update prisma

---
+++ latihan/hari8/rest-api-prisma/app.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const cors = require('cors')
+const express = require('express')
+const router = require('./src/routes/main')
+const app = express()
+const port = 3000
+
+var corsOptions = {
+  origin: process.env.CORS_ALLOW_LIST.split(','),
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use('/api', router)
+
+app.listen(port, () => {
+  console.log(`Example app listening on ${port}`)
+})
