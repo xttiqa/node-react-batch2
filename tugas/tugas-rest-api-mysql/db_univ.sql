37be128428d87272cdfbe446b15cde44fd82a983
xttiqa <attiqalucky@gmail.com>
2025-10-16 08:45:15 +0800

    upload sql db_univ

---
+++ tugas/tugas-rest-api-mysql/db_univ.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_univ
+-- ------------------------------------------------------
+-- Server version	8.0.43
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `nilai_mahasiswa`
+--
+
+DROP TABLE IF EXISTS `nilai_mahasiswa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `nilai_mahasiswa` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nama` varchar(192) NOT NULL,
+  `mata_kuliah` varchar(192) NOT NULL,
+  `nilai` int NOT NULL,
+  `indeks_nilai` varchar(4) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nilai_mahasiswa`
+--
+
+LOCK TABLES `nilai_mahasiswa` WRITE;
+/*!40000 ALTER TABLE `nilai_mahasiswa` DISABLE KEYS */;
+INSERT INTO `nilai_mahasiswa` VALUES (1,'Attiqa','Pemrograman dasar',76,'B','2025-10-15 15:52:37','2025-10-15 16:07:00'),(2,'Dhina','Pemrograman dasar',77,'B','2025-10-15 16:01:47','2025-10-15 16:01:47'),(4,'Naya','Pemrograman dasar',34,'E','2025-10-15 16:08:01','2025-10-15 16:08:01');
+/*!40000 ALTER TABLE `nilai_mahasiswa` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-10-16  8:44:52
